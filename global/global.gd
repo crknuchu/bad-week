d592d5b220b382c961fdfe11dba7f7fa28d1da8d
Bogdan <81476458+senseibobo@users.noreply.github.com>
2024-03-29 18:45:39 +0100

    folders

---
+++ global/global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func _input(event):
+	if Input.is_action_just_pressed("fullscreen"):
+		if get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
+			get_window().mode = Window.MODE_WINDOWED
+		else:
+			get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN
