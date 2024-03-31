54d7b7ab82ed708c2de5dd3d11689f6078fb126c
Bogdan <81476458+senseibobo@users.noreply.github.com>
2024-03-31 11:16:17 +0200

    fgjuy

--- interactables/letter/letter.gd
+++ interactables/letter/letter.gd
@@ -1,21 +1,11 @@
 extends Node3D
 
-@onready var letter = $Interactible
-@onready var letter_content = $"../Player/HUD/letter_content"
-@onready var maybe_read = $"../Player/HUD/maybe_read"
-@onready var not_sleepy = $"../Player/HUD/not_sleepy"
-@onready var shit = $"../Player/HUD/shit"
-var read_letter = false
+@onready var letter = $Interactable
+@export_multiline var text: String = "maybe i should readthat letter by the door"
 
 func _ready():
 	letter.interacted.connect(on_letter_interacted)
 	
 func on_letter_interacted():
+	Global.player.show_message(text, text.length()/20.0)
 	Global.player.take_letter()
-	letter_content.visible = true
-	await get_tree().create_timer(5).timeout
-	letter_content.visible = false
-	shit.visible = true
-	await get_tree().create_timer(3).timeout
-	shit.visible = false
-	self.queue_free()
--- levels/main_menu.gd
+++ levels/main_menu.gd
@@ -1,27 +1,8 @@
-extends Node3D
-
-@onready var hud = $HUD
-@onready var main_3d = $Main3D
-var level_instance
-
-
-func unload_level():
-	if(is_instance_valid(level_instance)):
-		level_instance.queue_free()
-	level_instance = null
-
-
-func load_level(level_path):
-	unload_level()
-	var level_resource = load(level_path)
-	if (level_resource):
-		level_instance = level_resource.instantiate()
-		main_3d.add_child(level_instance)
+extends Control
 
 
 func _on_start_pressed():
-	load_level("res://levels/level0/room.tscn")
-	hud.hide()
+	Transition.transition_to("res://levels/level0/room.tscn", 2.0)
 
 
 func _on_quit_pressed():
