68b856a2301c49ec3de854bab3daac72ff5e16ee
Bogdan <bogdanjsd@gmail.com>
2024-04-04 05:20:52 +0200

    flickering light work

--- non_interactables/flickering_light.gd
+++ non_interactables/flickering_light.gd
@@ -8,11 +8,19 @@ func _ready():
 	if not flickering and is_instance_valid(anim_player):
 		anim_player.play("RESET")
 		anim_player.queue_free()
+	elif flickering:
+		flicker()
+
+
+func flicker():
+	anim_player.play("flicker")
 
 
 func turn_on():
+	$AudioStreamPlayer3D.stop()
 	anim_player.play("turn_on")
 
 
 func turn_off():
+	$AudioStreamPlayer3D.stop()
 	anim_player.play("turn_off")
